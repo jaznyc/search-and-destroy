68e7569c1db59a2b8f40b6e9e619f51daa41b208
Jasmine Hatcher <jasminehatchernyc@gmail.com>
2020-10-28 21:07:26 -0400

    isloop & minJumps

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,9 +2,25 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
+  const nextCache = {};
+  let currNode = linkedlist.head;
+  let nextVal = currNode.next.value;
 
+  while (currNode !== null) {
+    let nextNode = currNode.next;
+    if (nextNode !== null) {
+      let nextVal = currNode.next.value;
+      if (nextCache.hasOwnProperty(nextVal)) {
+        return true;
+      } else {
+        nextCache[nextVal] = 0;
+      }
+    }
+    currNode = currNode.next;
+  }
+  console.log(nextCache);
+  return false;
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +30,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -3,6 +3,29 @@
 // Complete this algo
 const minJumps = arr => {
 
+  let length = arr.length 
+  let jumps = 0
+  let currPosition = 0
+ while(currPosition < length){
+
+    if(arr[currPosition]>=length-(currPosition+1)){
+        jumps=jumps+1
+        return jumps
+    }
+
+    let max=arr[currPosition+1]+1
+    let indexOfMax=currPosition+1
+    for(let i=1;i<=arr[currPosition];i++){
+        if(arr[currPosition+i]+i>max){
+            max=arr[currPosition+i]+i
+            indexOfMax=currPosition+i
+        }
+    }
+    //console.log(currPosition)
+    jumps=jumps+1
+    currPosition=indexOfMax
+
+}
 };
 
 module.exports = minJumps
